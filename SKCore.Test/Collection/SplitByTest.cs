d3873113a2bbe62949b3ee01e70d6d148573401a
Shun kanmera <s.kanmera@exzeal.co.jp>
2017-09-26 15:26:55 +0900

    First commit.

---
+++ SKCore.Test/Collection/SplitByTest.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SKCore.Collection.Test
+{
+    [TestClass]
+    public class SplitByTest
+    {
+        [TestMethod]
+        public void SplitBySizeWith3()
+        {
+            var source = new List<int> { 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6 };
+            var result = source.SplitBySize(3);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int> { 1, 1, 1 },
+                new List<int> { 2, 2, 3 },
+                new List<int> { 3, 3, 3 },
+                new List<int> { 4, 4, 5 },
+                new List<int> { 6, 6 },
+            }));
+        }
+
+        [TestMethod]
+        public void SplitBySizeWithEmpty()
+        {
+            var source = new List<int>();
+            var result = source.SplitBySize(3);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>()));
+        }
+
+        [TestMethod]
+        public void SplitByEquality()
+        {
+            var source = new List<int> { 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6 };
+            var result = source.SplitByEquality();
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int> { 1, 1, 1 },
+                new List<int> { 2, 2 },
+                new List<int> { 3, 3, 3, 3 },
+                new List<int> { 4, 4 },
+                new List<int> { 5 },
+                new List<int> { 6, 6 },
+            }));
+        }
+
+        [TestMethod]
+        public void SplitByEqualityWithMaxSize()
+        {
+            var source = new List<int> { 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6 };
+            var result = source.SplitByEquality(3);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int> { 1, 1, 1 },
+                new List<int> { 2, 2 },
+                new List<int> { 3, 3, 3 },
+                new List<int> { 3 },
+                new List<int> { 4, 4 },
+                new List<int> { 5 },
+                new List<int> { 6, 6 },
+            }));
+        }
+
+        [TestMethod]
+        public void SplitByEqualityWithEmpty()
+        {
+            var source = new List<int>();
+            var result = source.SplitByEquality();
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>()));
+        }
+
+        [TestMethod]
+        public void SplitByRegulalityConditionIncrease()
+        {
+            var source = new List<int> { 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6, 1, 1, 2, 3, 3, 3, 4 };
+            var result = source.SplitByRegularity((p, c, gi, li) => p <= c);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int>{ 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 5, 6, 6,},
+                new List<int>{ 1, 1, 2, 3, 3, 3, 4 },
+            }));
+        }
+
+        [TestMethod]
+        public void SplitByRegulalityConditionSerialNumber()
+        {
+            var source = new List<int> { 1, 2, 3, 5, 6, 5, 6, 7, 8, -2, -1, 0, 2 };
+            var result = source.SplitByRegularity((p, c, gi, li) => c == p + 1);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int> { 1, 2, 3 },
+                new List<int> { 5, 6 },
+                new List<int> { 5, 6, 7, 8 },
+                new List<int> { -2, -1, 0 },
+                new List<int> { 2 },
+            }));
+        }
+
+        [TestMethod]
+        public void SplitByRegulalityConditionLessOrEqualGlobalIndex()
+        {
+                        /* GlobalIndex   0  1  2  3  4   5  6  7  8  9  10  11  12  13  14  15  16  17  18  */
+            var source = new List<int> { 0, 0, 1, 1, 5, -1, 6, 0, 9, 2,  1,  3,  4, 20,  1,  0, 30, 18,  0,};
+            var result = source.SplitByRegularity((p, c, gi, li) => c <= gi);
+
+            Assert.IsTrue(result.NestedSequenceEqual(new List<List<int>>
+            {
+                new List<int> { 0, 0, 1, 1 },
+                new List<int> { 5, -1, 6, 0 },
+                new List<int> { 9, 2, 1, 3, 4 },
+                new List<int> { 20, 1, 0 },
+                new List<int> { 30 },
+                new List<int> { 18, 0 },
+            }));
+        }
+    }
+}
---
+++ SKCore/Collection/SplitBy.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKCore.Collection
+{
+    public static partial class EnumerableExtension
+    {
+        public static IEnumerable<IEnumerable<T>> SplitBySize<T>(
+            this IEnumerable<T> source, int size)
+        {
+            return source.SplitByRegularity((p, c, gi, li) => li < size);
+        }
+
+        public static IEnumerable<IEnumerable<T>> SplitByEquality<T>(
+            this IEnumerable<T> source)
+        {
+            return source.SplitByRegularity((p, c, gi, li) => c.Equals(p));
+        }
+
+        public static IEnumerable<IEnumerable<T>> SplitByEquality<T>(
+            this IEnumerable<T> source, int maxSize)
+        {
+            return source.SplitByRegularity((p, c, gi, li) => c.Equals(p) && li < maxSize);
+        }
+
+        public static IEnumerable<IEnumerable<T>> SplitByEquality<T>(
+            this IEnumerable<T> source, IEqualityComparer<T> comparer)
+        {
+            return source.SplitByRegularity((p, c, gi, li) => comparer.Equals(c, p));
+        }
+
+        public static IEnumerable<IEnumerable<T>> SplitByEquality<T>(
+            this IEnumerable<T> source, IEqualityComparer<T> comparer, int maxSize)
+        {
+            return source.SplitByRegularity((p, c, gi, li) => comparer.Equals(c, p) && li <= maxSize);
+        }
+
+        public static IEnumerable<IEnumerable<T>> SplitByRegularity<T>(
+            this IEnumerable<T> source, Func<T, T, int, int, bool> predicate)
+        {
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext())
+                    yield break;
+
+                var items = new List<T> { enumerator.Current };
+                var localIndex = 0;
+                var globalIndex = 0;
+                var previous = default(T);
+                while (true)
+                {
+                    previous = enumerator.Current;
+                    if (!enumerator.MoveNext())
+                        break;
+
+                    if (predicate(previous, enumerator.Current, ++globalIndex, ++localIndex))
+                    {
+                        items.Add(enumerator.Current);
+                        continue;
+                    }
+
+                    yield return items;
+
+                    localIndex = 0;
+                    items = new List<T> { enumerator.Current };
+                }
+
+                if (items.Any())
+                    yield return items;
+            }
+        }
+    }
+}
+
