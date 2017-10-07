ad44b0c5f87982975f0c392b00e3d7ca4dd0bde5
s.kanmera <s.kanmera.dev@gmail.com>
2017-10-07 18:00:39 +0900

    Add times extension method.

---
+++ SKCore/Collection/Times.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SKCore.Collection
+{
+    public static partial class EnumerableExtension
+    {
+        public static void Times(this int count, Action action)
+        {
+            count.Times(0, i => action());
+        }
+
+        public static void Times(this int count, Action<int> action)
+        {
+            count.Times(0, action);
+        }
+
+        public static void Times(this int count, int start, Action<int> action)
+        {
+            if (count > 0)
+                for (int i = start, end = start + count; i < end; i++)
+                    action(i);
+            else
+                for (int i = start, end = start + count; i > end; i--)
+                    action(i);
+        }
+    }
+}
