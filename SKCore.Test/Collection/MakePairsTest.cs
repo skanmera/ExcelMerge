8b198ab540eb688ea35cd5d385380b1e668c4fea
Shun kanmera <s.kanmera@exzeal.co.jp>
2017-10-12 20:04:58 +0900

    Add MakePairs extension method.

---
+++ SKCore.Test/Collection/MakePairsTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SKCore.Collection;
+
+namespace SKCore.Test.Collection
+{
+    [TestClass]
+    public class MakePairsTest
+    {
+        [TestMethod]
+        public void MakePairsTestNormal()
+        {
+            var source = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var result = source.MakePairs().ToList();
+
+            Assert.IsTrue(result.SequenceEqual(new List<Tuple<int, int>>
+            {
+                Tuple.Create(1,2),
+                Tuple.Create(2,3),
+                Tuple.Create(3,4),
+                Tuple.Create(4,5),
+                Tuple.Create(5,6),
+            }, new TupleEqualityCompare<int>()));
+        }
+
+        [TestMethod]
+        public void MakePairsTestWithEmpty()
+        {
+            var source = new List<int>();
+            var result = source.MakePairs().ToList();
+
+            Assert.IsTrue(result.SequenceEqual(new List<Tuple<int, int>>(), new TupleEqualityCompare<int>()));
+        }
+
+        [TestMethod]
+        public void MakePairsTestGivenOddElementCount()
+        {
+            var source = new List<int> { 1, 2, 3, 4, 5 };
+            var result = source.MakePairs().ToList();
+
+            Assert.IsTrue(result.SequenceEqual(new List<Tuple<int, int>>
+            {
+                Tuple.Create(1,2),
+                Tuple.Create(2,3),
+                Tuple.Create(3,4),
+                Tuple.Create(4,5),
+            }, new TupleEqualityCompare<int>()));
+        }
+    }
+
+    class TupleEqualityCompare<T> : IEqualityComparer<Tuple<T, T>>
+    {
+        public bool Equals(Tuple<T, T> x, Tuple<T, T> y)
+        {
+            return GetHashCode(x).Equals(GetHashCode(y));
+        }
+
+        public int GetHashCode(Tuple<T, T> obj)
+        {
+            return (obj.Item1?.GetHashCode() ?? GetHashCode()) ^ (obj.Item2?.GetHashCode() ?? GetHashCode());
+        }
+    }
+}
---
+++ SKCore/Collection/MakePairs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SKCore.Collection
+{
+    public static partial class EnumerableExtension
+    {
+        public static IEnumerable<Tuple<T, T>> MakePairs<T>(this IEnumerable<T> source)
+        {
+            return source.SplitByRegularity((items, current) => { items.Add(current); return false; })
+                .Where(i => i.Count() > 1)
+                .Select(i => Tuple.Create(i.ElementAt(0), i.ElementAt(1)));
+        }
+    }
+}
