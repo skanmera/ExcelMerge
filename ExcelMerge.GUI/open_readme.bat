2f8b5da063f52cc521479b5a4db3854b43676dcd
s.kanmera <s.kanmera.dev@gmail.com>
2017-10-29 15:48:01 +0900

    Open README.md when finished install.

--- ExcelMerge.GUI/open_readme.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for /f "tokens=3 delims= " %%a in ('chcp') do set lang=%%a
-
-if "%lang%" == "932" ( 
-    start "" https://github.com/skanmera/ExcelMerge/blob/master/README.jp.md#readme ) else ( 
-    start "" https://github.com/skanmera/ExcelMerge/blob/master/README.md#readme )
