7fad209aa6c85ba527b13b918e88796edd146fca
skanmera <s.kanmera.dev@gmail.com>
2018-03-20 19:47:45 +0900

    Add a function to copy a directory tree.

---
+++ IO/PathUtility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SKCore.IO
+{
+    public static class PathUtility
+    {
+        public static void CopyTree(string src, string dst, bool overwrite = false)
+        {
+            var fileInfo = new FileInfo(dst);
+            fileInfo.Directory.Create();
+
+            File.Copy(src, dst, overwrite);
+        }
+    }
+}
