c523223a03a31aad24608dd7ea8e441dd122f1e0
skanmera <s.kanmera.dev@gmail.com>
2018-03-20 17:14:36 +0900

    Squashed 'NetDiff/' changes from 99b44d5..45a36a2
    45a36a2 Change optimization logic to reduce redundant differences. Concurrently change tests.
    
    git-subtree-dir: NetDiff
    git-subtree-split: 45a36a2e595acc7be94bc33a09e9dde4ceb9049a

---
+++ NetDiff/DiffResultExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace NetDiff
+{
+    public static class DiffResultExtension
+    {
+        public static IEnumerable<T> CreateSrc<T>(
+            this IEnumerable<DiffResult<T>> self)
+        {
+            return DiffUtil.CreateSrc(self);
+        }
+
+        public static IEnumerable<T> CreateDst<T>(
+            this IEnumerable<DiffResult<T>> self)
+        {
+            return DiffUtil.CreateDst(self);
+        }
+
+        public static IEnumerable<DiffResult<T>> Optimize<T>(
+            this IEnumerable<DiffResult<T>> self, IEqualityComparer<T> compare = null)
+        {
+            return DiffUtil.Optimaize(self, compare);
+        }
+
+        public static IEnumerable<DiffResult<T>> Order<T>(
+            this IEnumerable<DiffResult<T>> self, DiffOrderType orderType)
+        {
+            return DiffUtil.Order(self, orderType);
+        }
+    }
+}
