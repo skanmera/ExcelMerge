656862c895f8fa8d9adc42aa2f156b47956fbb22
skanmera <s.kanmera.dev@gmail.com>
2018-03-20 17:32:07 +0900

    Create emprty rows if blank(null) rows are founded in the middle of data.

--- ExcelMerge/ExcelReader.cs
+++ ExcelMerge/ExcelReader.cs
@@ -1,26 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NPOI.SS.UserModel;
 
 namespace ExcelMerge
 {
     internal class ExcelReader
     {
-        internal static IEnumerable<ExcelRow> Read(ISheet sheet)
+        public static IEnumerable<ExcelRow> Read(ISheet sheet)
         {
             var actualRowIndex = 0;
             for (int rowIndex = 0; rowIndex <= sheet.LastRowNum; rowIndex++)
             {
                 var row = sheet.GetRow(rowIndex);
-                if (row == null)
-                    continue;
 
                 var cells = new List<ExcelCell>();
-                for (int columnIndex = 0; columnIndex < row.LastCellNum; columnIndex++)
+                if (row != null)
                 {
-                    var cell = row.GetCell(columnIndex);
-                    var stringValue = ExcelUtility.GetCellStringValue(cell);
+                    for (int columnIndex = 0; columnIndex < row.LastCellNum; columnIndex++)
+                    {
+                        var cell = row.GetCell(columnIndex);
+                        var stringValue = ExcelUtility.GetCellStringValue(cell);
 
-                    cells.Add(new ExcelCell(stringValue, columnIndex, rowIndex));
+                        cells.Add(new ExcelCell(stringValue, columnIndex, rowIndex));
+                    }
                 }
 
                 yield return new ExcelRow(actualRowIndex++, cells);
