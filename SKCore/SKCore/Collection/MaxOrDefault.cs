ead6edbe25bd5929aa8bdd3b9a8684734dd4c115
skanmera <s.kanmera.dev@gmail.com>
2018-03-23 15:54:52 +0900

    Add extension methods.

---
+++ SKCore/SKCore/Collection/MaxOrDefault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKCore.Collection
+{
+    public static partial class EnumerableExtension
+    {
+        public static int MaxOrDefault<T>(
+            this IEnumerable<T> source, Func<T, int> selector, int defaultValue = 0)
+        {
+            return source.Any() ? source.Max(selector) : defaultValue;
+        }
+    }
+}
---
+++ SKCore/SKCore/Collection/MinOrDefault.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKCore.Collection
+{
+    public static partial class EnumerableExtension
+    {
+        public static int MinOrDefault<T>(
+            this IEnumerable<T> source, Func<T, int> selector, int defaultValue = 0)
+        {
+            return source.Any() ? source.Min(selector) : defaultValue;
+        }
+    }
+}
